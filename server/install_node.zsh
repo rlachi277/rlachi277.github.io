5fc938e881c6c67114f7f667377ab156b576dee6
rlachi277 <jun_1103@icloud.com>
2026-03-03 23:08:02 +0900

    hi my name is node.js nice to meet you i'm fine thank you and you? (ssr 구현하려면 serialize 뜯어고쳐야 돼서 정신 나감)

---
+++ server/install_node.zsh
@@ -0,0 +1,8 @@
+# 맥 말고 다른 환경에선 안 먹힐 가능성이 매우 높음
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install 24
--- server/venv.zsh
+++
@@ -1,1 +0,0 @@
-source _back/bin/activate
