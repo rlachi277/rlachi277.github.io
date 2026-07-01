a011aaff047d6effa74b421192cd372c5d10fa9f
rlachi277 <jun_1103@icloud.com>
2026-07-01 23:42:31 +0900

    oh I should ignore dist

--- dist/vibing/chat/keyboard.js
+++
@@ -1,7 +0,0 @@
-export function getMessageEnterAction(event, composing) {
-    if (event.key !== "Enter" || event.shiftKey)
-        return "default";
-    if (composing || event.isComposing || event.keyCode === 229)
-        return "compose";
-    return "send";
-}
